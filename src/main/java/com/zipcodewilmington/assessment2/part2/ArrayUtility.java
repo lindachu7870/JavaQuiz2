f578ebff5014c52a41ee2d8951caaba333e96bef
Meilin Li (Linda) <limeilinda@gmail.com>
2022-07-16 16:15:08 -0400

    passed 6 of 8 tests in part 3

--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -1,18 +1,30 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import java.util.Arrays;
+
 public class ArrayUtility {
+    // given two arrays, create a new array with elements from both array. Return the new array
     public Integer[] merge(Integer[] array1, Integer[] array2) {
-        return null;
+       Integer[] mergeArr = Arrays.copyOf(array1, array1.length + array2.length);
+       System.arraycopy(array2, 0, mergeArr, array1.length, array2.length);
+       return mergeArr;
     }
 
+    // given an array and a number, rotate the content to the left as many times as the number given. Return the new rotated array.
     public Integer[] rotate(Integer[] array, Integer index) {
+//        Integer[] rotateLeft = new Integer[array.length];
+//        for (int i = 0; i < array.length; i++) {
+//            }
+//        }
         return null;
     }
 
+    // count how many times a number appear in both arrays
     public Integer countOccurrence(Integer[] array1, Integer[] array2, Integer valueToEvaluate) {
         return null;
     }
 
+    // return the element that appears in the array the most.
     public Integer mostCommon(Integer[] array) {
         return null;
     }
--- src/main/java/com/zipcodewilmington/assessment2/part3/RedRobin.java
+++ src/main/java/com/zipcodewilmington/assessment2/part3/RedRobin.java
@@ -1,4 +1,27 @@
 package com.zipcodewilmington.assessment2.part3;
 
-public class RedRobin {
+public class RedRobin extends Bird {
+
+    // field
+    private String migrationMonth;
+
+    // getter
+    public String getMigrationMonth() {
+        return migrationMonth;
+    }
+
+    // setter
+    public void setMigrationMonth() {
+        this.migrationMonth = migrationMonth;
+    }
+
+    @Override
+    public int getSpeed() {
+        return 10;
+    }
+
+    @Override
+    public String color() {
+        return "red";
+    }
 }
