619f3234b416065310bad71a2237671e2e1d83f5
Meilin Li (Linda) <limeilinda@gmail.com>
2022-07-16 14:34:21 -0400

    pushing this into macbook

--- src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
@@ -5,16 +5,16 @@ public class IntegerArrayUtilities {
         return array.length % 2 == 0;
     }
 
+    // `Range` should return an array of integers from `start` to `stop` inclusively.
     public Integer[] range(int start, int stop) {
-
         return null;
     }
 
     public Integer getSumOfFirstTwo(Integer[] array) {
-        return null;
+        return array[0] + array[1];
     }
 
     public Integer getProductOfFirstTwo(Integer[] array) {
-        return null;
+        return array[0] * array[1];
     }
 }
--- src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
@@ -1,10 +1,20 @@
 package com.zipcodewilmington.assessment2.part1;
 
+/*
+Create a class `WuTangConcatenator` which uses a composite `Integer` to identify divisibility.
+    * Multiples of `3` are considered `Wu` numbers.
+    * Multiples of `5` are considered `Tang` numbers.
+    * Multiples of `3` and `5` are considered `WuTang` numbers.
+    A composite number is a positive integer that can be formed by multiplying two smaller positive integers.
+ */
+
 public class WuTangConcatenator {
     public WuTangConcatenator(Integer input) {
+        
     }
 
     public Boolean isWu() {
+
         return null;
     }
 
